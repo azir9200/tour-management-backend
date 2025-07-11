3fbfc990d69812c7c25c46df56f6efb6710aab37
Azir@9200 <aziruddin83@hotmail.com>
2025-07-11 20:43:39 +0100

    hashed password created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 import { UserControllers } from "./user.controller";
 
 const router = Router();
+
 router.post("/register", UserControllers.createUser);
+
 router.get("/", UserControllers.getAllUsers);
+
+router.patch("/:id", UserControllers.updateUser);
 
 export const UserRoutes = router;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,21 +1,33 @@
+import bcryptjs from "bcryptjs";
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
+import { envVars } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = async (payload: Partial<IUser>) => {
   const { email, password, ...rest } = payload;
 
-  const isUserExist = await User.findOne({ email });
+  const isUserExist = await User.create({ email });
 
   if (isUserExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist");
   }
+  const hashedPassword = await bcryptjs.hash(
+    password as string,
+    Number(envVars.BCRYPT_SALT_ROUND)
+  );
+
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
 
   const user = await User.create({
     email,
-    password,
-
+    password: hashedPassword,
+    auths: [authProvider],
     ...rest,
   });
 
@@ -23,18 +35,59 @@ const createUser = async (payload: Partial<IUser>) => {
 };
 
 const getAllUsers = async () => {
-    const users = await User.find({});
-    const totalUsers = await User.countDocuments();
-    return {
-        data: users,
-        meta: {
-            total: totalUsers
-        }
-    }
+  const users = await User.find({});
+  const totalUsers = await User.countDocuments();
+  return {
+    data: users,
+    meta: {
+      total: totalUsers,
+    },
+  };
 };
 
+const updateUser = async (
+  userId: string,
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload
+) => {
+  const ifUserExists = await User.findById(userId);
+
+  if (!ifUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+  }
+  if (payload.role) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+
+    if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.isActive || payload.isDeleted || payload.isVerified) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.password) {
+    payload.password = await bcryptjs.hash(
+      payload.password,
+      envVars.BCRYPT_SALT_ROUND
+    );
+  }
+
+  const newUpdatedUser = await User.findByIdAndUpdate(userId, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return newUpdatedUser;
+};
 
 export const UserServices = {
   createUser,
   getAllUsers,
+  updateUser,
 };
