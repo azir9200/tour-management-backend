cf121161e4d5bbc38092d4fd447e9fc4cd576354
Azir@9200 <aziruddin83@hotmail.com>
2025-07-14 08:09:51 +0100

    declare user as a global interface

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,4 @@
-
-import { NextFunction,  Request,  Response } from "express";
+import { Request, Response } from "express";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
