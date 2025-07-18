8791458a8cdcb7cdedd571a35f6d99e9ee51fff1
Azir@9200 <aziruddin83@hotmail.com>
2025-07-18 20:42:22 +0100

    Ignore .env file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import { envVars } from "./app/config/env";
 
 let server: Server;
-const port = 5000;
 
 const startServer = async () => {
   try {
