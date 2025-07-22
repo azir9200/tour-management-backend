cd59d27a3287f025895b6810ea9a62423b544c2c
Azir@9200 <aziruddin83@hotmail.com>
2025-07-22 08:22:01 +0100

    passport authentication created

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -26,7 +26,7 @@ const loadEnvVariables = (): EnvConfig => {
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
-            throw new Error(`Missing require environment variabl ${key}`)
+            throw new Error(`Missing require environment variable ${key}`)
         }
     })
 
